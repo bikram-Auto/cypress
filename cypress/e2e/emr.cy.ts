f26b489c5ed9ae768d6de849e54ca1836a588fd9
Bikram <bikram.nanda@squbix.com>
2024-08-05 18:07:16 +0530

    update test class name and add packages

--- cypress/e2e/emr.cy.ts
+++ cypress/e2e/emr.cy.ts
@@ -1,21 +1,29 @@
 
-const { Base } = require("./utils/base.ts")
+
+import { Base } from "./utils/base.ts";
 
 const base = new Base();
 
 const email = 'ayaan.singh@sharklasers.com';
 const password = '12345678';
 
-describe('EMR Test Case', () => {
-  it('Data Retention', () => {
-    cy.visit("https://akhilhistest.squbix.com/")
+
+
+describe('EMR test cases', () => {
+  beforeEach(() => {
+    console.log('uri: ', process.env.Base_uri)
+    const url = process.env.Base_uri;
+    // cy.visit(url) 
     base.doctorLogin(cy, email, password)
+  })
+
+  it('Data Retention', () => {
     cy.wait(500)
-    cy.get("button[type='button']").click()
+    cy.get("button[type='button']").click();
     cy.wait(1000)
     elements(cy.get('.row.text-start.cursor-pointer.hover-effect.border-bottom').contains('ARADHYA')).eq(0).click();
-    base.vitals(cy)
-    base.ChiefComplaints(cy)
+    base.vitals(cy);
+    base.ChiefComplaints(cy);
     // cy.get('button').contains('Draft').click();
     // cy.get("div[role='alert']").should('contain','Updated Successfully');
     // cy.reload();
@@ -24,7 +32,7 @@ describe('EMR Test Case', () => {
 
 
 
-function elements(ele) {
+function elements(ele: any) {
   return ele;
 }
 
